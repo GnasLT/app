c0ef2e82d9fc04d6a2ed7e9d4d0c90ba08c2f366
GnasLT <143161977+GnasLT@users.noreply.github.com>
2025-05-13 17:57:40 +0700

    Update PlantIndex.py

--- PlantIndex.py
+++ PlantIndex.py
@@ -3,17 +3,25 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class PlantIndex:
-    def __init__(self,rgb,nir):
-        self.rgb = rgb
-        self.nir = nir
-        self.ndvi = self.analysis()
+    def __init__(self,image2analysis,time):
+        self.image2analysis = image2analysis
+        self.time = time
         self.collection = self.get_collection()
     def get_collection(self):
         self.client = DBconnect.DBconnect()
         db = self.client.getdb()
         return db['PlantIndex']    
-        
-    def analysis(self,img_nir,img_rgb,time):
+    
+    def analysis_save_plant_index(self):
+         data = {
+                'image2analysis': self.image2analysis,
+                'time': self.time,
+                'NDVI': self.analysis,
+            }
+        self.collection.insert_one(data)
+    def analysis(self,img_nir,img_rgb):
+        self.img_nir = img_nir
+        self.img_rgb = img_rgb
         height, width, channels = img_nir.shape # h = 2464, w = 3280
 
         img_rgb = cv2.resize(img_rgb,(width,height),cv2.INTER_CUBIC)
@@ -39,7 +47,7 @@ class PlantIndex:
             where=(nir + red) != 0
         )
         plt.imsave(
-            f'ndvi_{time}.png',          
+            f'/Desktop/thesis/images/ndvi_{time}.png',          
             ndvi,                       
             cmap='RdYlGn',              
             vmin=-1,                    
@@ -47,11 +55,12 @@ class PlantIndex:
             origin='upper',             
             dpi=300                     
         )
-        
-    def image_alignment(self,img_nir,img_rgb):
+    return f'/Desktop/thesis/images/ndvi_{time}.png'
+    
+    def image_alignment(self):
         sift = cv2.SIFT_create()
-        kp1, des1 = sift.detectAndCompute(img_nir, None)
-        kp2, des2 = sift.detectAndCompute(img_rgb, None)
+        kp1, des1 = sift.detectAndCompute(self.img_nir, None)
+        kp2, des2 = sift.detectAndCompute(self.img_rgb, None)
         
         FLANN_INDEX_KDTREE = 1
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
@@ -71,5 +80,5 @@ class PlantIndex:
         H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
         filtered_matches = filter_matches_with_mask(good_matches, mask)
         
-        img_warp = cv2.warpPerspective(img_nir, H, (width,height))
+        img_warp = cv2.warpPerspective(self.img_nir, H, (width,height))
         return img_warp
